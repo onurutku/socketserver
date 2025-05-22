3782845db8f0c12bcdf2d0b4e8ee2591ba741b51
Onur Utku Topaloğlu <47296136+onurutku@users.noreply.github.com>
2025-05-22 13:05:43 +0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,53 +1,45 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-var fs = require( 'fs' );
-const options = {
-        //key: fs.readFileSync('private.key'),
-        cert: fs.readFileSync('certificate.crt'),
-        ca: fs.readFileSync('ca_bundle.crt'),
-        requestCert: false,
-        rejectUnauthorized: false
-    }
-const http = require('https').createServer(options,app);
-// const http = require('http').Server(app);
-// const PORT = 5000
-const PORT = 443
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "*"
-    }
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http").createServer(app);
+const PORT = process.env.PORT || 5000;
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
 });
 
-app.use(cors())
-let users = []
+app.use(cors());
 
-socketIO.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`)
-    
-    socket.on("message", data => {
-      socketIO.emit("messageResponse", data)
-    })
+let users = [];
 
-    socket.on("typing", data => (
-      socket.broadcast.emit("typingResponse", data)
-    ))
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
 
-    socket.on("newUser", data => {
-      users.push(data)
-      socketIO.emit("newUserResponse", users)
-    })
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-      users=users.filter(user=>(user.id!==socket.id)||(!user.id));
-      socketIO.emit('disconnected',users)
-      socket.disconnect()
-    });
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+  });
+
+  socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
+
+  socket.on("newUser", (data) => {
+    users.push(data);
+    socketIO.emit("newUserResponse", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    users = users.filter((user) => user.id !== socket.id || !user.id);
+    socketIO.emit("disconnected", users);
+    socket.disconnect();
+  });
 });
 
 app.get("/users", (req, res) => {
-  res.json(users)
+  res.json(users);
 });
+
 http.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
